7abd0b925f7b710dcadb6ad0a41ffb42b87e61e8
sasaki404 <takaaki.sasaki2001@gmail.com>
2024-01-28 18:18:30 +0900

    fix record format

--- lib/model/daily_record.dart
+++ lib/model/daily_record.dart
@@ -1,3 +1,5 @@
+import 'package:darts_record_app/page/game/logic/calculator.dart';
+
 class DailyRecord {
   final int id;
   final int userId;
@@ -69,13 +71,100 @@ class DailyRecord {
     Map<DateTime, List<String>> event = {};
     String val = "";
     for (var record in records) {
-      for (var e in record.countMap.entries) {
-        val += "${e.key} : ${e.value}count\n";
+      List<String> sortedKeys = record.countMap.keys.toList()
+        ..sort((a, b) => _customCompare(a, b));
+      Map<String, int> sortedMap = {
+        for (var key in sortedKeys) key: record.countMap[key]!
+      };
+      int sum = Calculator.sumCountMap(record.countMap);
+      val += "total throw count : ${sum}\n";
+      for (var e in sortedMap.entries) {
+        val += "'${e.key}' : ${e.value}";
+        if (!Calculator.isAward(e.key)) {
+          val += "(${(100 * e.value / sum).toStringAsFixed(2)}%)";
+        }
+        val += ", ";
       }
       final date = record.targetDate.split("-");
       event[DateTime.utc(
           int.parse(date[0]), int.parse(date[1]), int.parse(date[2]))] = [val];
     }
     return event;
+  }
+
+  static int _customCompare(String a, String b) {
+    List<String> order = [
+      'DOUBLE-1',
+      'DOUBLE-2',
+      'DOUBLE-3',
+      'DOUBLE-4',
+      'DOUBLE-5',
+      'DOUBLE-6',
+      'DOUBLE-7',
+      'DOUBLE-8',
+      'DOUBLE-9',
+      'DOUBLE-10',
+      'DOUBLE-11',
+      'DOUBLE-12',
+      'DOUBLE-13',
+      'DOUBLE-14',
+      'DOUBLE-15',
+      'DOUBLE-16',
+      'DOUBLE-17',
+      'DOUBLE-18',
+      'DOUBLE-19',
+      'DOUBLE-20',
+      'TRIPLE-1',
+      'TRIPLE-2',
+      'TRIPLE-3',
+      'TRIPLE-4',
+      'TRIPLE-5',
+      'TRIPLE-6',
+      'TRIPLE-7',
+      'TRIPLE-8',
+      'TRIPLE-9',
+      'TRIPLE-10',
+      'TRIPLE-11',
+      'TRIPLE-12',
+      'TRIPLE-13',
+      'TRIPLE-14',
+      'TRIPLE-15',
+      'TRIPLE-16',
+      'TRIPLE-17',
+      'TRIPLE-18',
+      'TRIPLE-19',
+      'TRIPLE-20',
+      '1',
+      '2',
+      '3',
+      '4',
+      '5',
+      '6',
+      '7',
+      '8',
+      '9',
+      '10',
+      '11',
+      '12',
+      '13',
+      '14',
+      '15',
+      '16',
+      '17',
+      '18',
+      '19',
+      '20',
+      'S-BULL',
+      'D-BULL',
+      'LOW TON',
+      'HAT TRICK',
+      'HIGH TON',
+      'TON80',
+      'THREE IN THE BLACK'
+    ];
+
+    int indexA = order.indexOf(a);
+    int indexB = order.indexOf(b);
+    return indexB - indexA;
   }
 }
